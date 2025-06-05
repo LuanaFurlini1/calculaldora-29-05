546fca982298d12afa30e0cec520ced5423f13e8
LuanaFurlini1 <luanafurliniestudos@gmail.com>
2025-06-05 20:21:23 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,28 +1,46 @@
-n = int(input("Digie um número: "))
-op = input("Digite a operação (+, -, *, / ou = para sair): ")
+for i in range(5):
+    user = input("Informe seu nome de usuario: ")
+    pas = int(input("Informe sua senha: "))
 
-while op != "=":
-    n2 = int(input("Digite outro número: "))
+    if user == "Luana" and pas == 1234:
+        n = int(input("Digie um número: "))
+        op = input("Digite a operação (+, -, *, / ou = para sair): ")
 
-    if op == "+":
-        n = n + n2
+        while op != "=":
+            n2 = int(input("Digite outro número: "))
 
-    elif op == "-":
-        n = n - n2
+            if op == "+":
+                n = n + n2
 
-    elif op == "*":
-        n = n * n2
+            elif op == "-":
+                n = n - n2
 
-    elif op == "/":
-        if n2 != 0:
-            n = n / n2
+            elif op == "*":
+                n = n * n2
+
+            elif op == "/":
+                if n2 != 0:
+                    n = n / n2
+                else:
+                    print("Impossivel dividir por 0!")
+                    break
+            else:
+                print("Operador invalido!")
+                break
+
+            op = input("Digite a operação: ")
+
+        user = input("Informe seu nome de usuario: ")
+        pas = int(input("Informe sua senha: "))
+
+        if user == "Luana" and pas == 1234:
+            print(f"Resultado = {n}")
+
         else:
-            print("Impossivel dividir por 0!")
-            break
-    else:
-        print("Operador invalido!")
+            print("Acesso negado!")
         break
 
-    op = input("Digite a operação: ")
+    else:
+        print("Usuario ou senha incorretos!")
 
-print(f"Resultado = {n}")
+
